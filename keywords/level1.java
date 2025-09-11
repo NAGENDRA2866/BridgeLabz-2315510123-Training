b50ca0426d0471b90c0a073587bfbba506738981
NAGENDRA2866 <mirthipatinagendra09@gmail.com>
2025-09-11 10:45:05 +0530

    Add sample programs: BankAccount, Book, Employee, Product, Student, Vehicle, Patient

---
+++ keywords/level1.java
@@ -0,0 +1,212 @@
+// Bank Account System
+
+class BankAccount {
+    static String bankName = "Global Bank";
+    private static int totalAccounts = 0;
+    final int accountNumber;
+    String accountHolderName;
+
+    BankAccount(int accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        totalAccounts++;
+    }
+
+    static void getTotalAccounts() {
+        System.out.println("Total accounts: " + totalAccounts);
+    }
+
+    void displayAccountDetails() {
+        if (this instanceof BankAccount) {
+            System.out.println("Bank: " + bankName);
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Account Holder: " + accountHolderName);
+        }
+    }
+}
+
+
+// Library Management System 
+
+class Book {
+    static String libraryName = "City Library";
+    final String isbn;
+    String title;
+    String author;
+
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    static void displayLibraryName() {
+        System.out.println("Library Name: " + libraryName);
+    }
+
+    void displayBookDetails() {
+        if (this instanceof Book) {
+            System.out.println("ISBN: " + isbn);
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+        }
+    }
+}
+
+
+// Employee Management System
+
+class Employee {
+    static String companyName = "Tech Corp";
+    private static int totalEmployees = 0;
+    final int id;
+    String name;
+    String designation;
+
+    Employee(int id, String name, String designation) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        totalEmployees++;
+    }
+
+    static void displayTotalEmployees() {
+        System.out.println("Total Employees: " + totalEmployees);
+    }
+
+    void displayEmployeeDetails() {
+        if (this instanceof Employee) {
+            System.out.println("Company: " + companyName);
+            System.out.println("ID: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Designation: " + designation);
+        }
+    }
+}
+
+
+// Shopping Cart System
+
+class Product {
+    static double discount = 10.0;  // percentage
+    final int productID;
+    String productName;
+    double price;
+    int quantity;
+
+    Product(int productID, String productName, double price, int quantity) {
+        this.productID = productID;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    static void updateDiscount(double newDiscount) {
+        discount = newDiscount;
+    }
+
+    void displayProductDetails() {
+        if (this instanceof Product) {
+            System.out.println("Product ID: " + productID);
+            System.out.println("Product Name: " + productName);
+            System.out.println("Price: " + price);
+            System.out.println("Quantity: " + quantity);
+            System.out.println("Discount: " + discount + "%");
+        }
+    }
+}
+
+
+// University Student Management
+
+class Student {
+    static String universityName = "State University";
+    private static int totalStudents = 0;
+    final int rollNumber;
+    String name;
+    String grade;
+
+    Student(int rollNumber, String name, String grade) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.grade = grade;
+        totalStudents++;
+    }
+
+    static void displayTotalStudents() {
+        System.out.println("Total Students: " + totalStudents);
+    }
+
+    void displayStudentDetails() {
+        if (this instanceof Student) {
+            System.out.println("University: " + universityName);
+            System.out.println("Roll Number: " + rollNumber);
+            System.out.println("Name: " + name);
+            System.out.println("Grade: " + grade);
+        }
+    }
+}
+
+
+// Vehicle Registration System
+
+class Vehicle {
+    static double registrationFee = 500.0;
+    final String registrationNumber;
+    String ownerName;
+    String vehicleType;
+
+    Vehicle(String registrationNumber, String ownerName, String vehicleType) {
+        this.registrationNumber = registrationNumber;
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+
+    void displayVehicleDetails() {
+        if (this instanceof Vehicle) {
+            System.out.println("Registration Number: " + registrationNumber);
+            System.out.println("Owner Name: " + ownerName);
+            System.out.println("Vehicle Type: " + vehicleType);
+            System.out.println("Registration Fee: " + registrationFee);
+        }
+    }
+}
+
+
+// Hospital Management System
+
+class Patient {
+    static String hospitalName = "City Hospital";
+    private static int totalPatients = 0;
+    final int patientID;
+    String name;
+    int age;
+    String ailment;
+
+    Patient(int patientID, String name, int age, String ailment) {
+        this.patientID = patientID;
+        this.name = name;
+        this.age = age;
+        this.ailment = ailment;
+        totalPatients++;
+    }
+
+    static void getTotalPatients() {
+        System.out.println("Total Patients: " + totalPatients);
+    }
+
+    void displayPatientDetails() {
+        if (this instanceof Patient) {
+            System.out.println("Hospital: " + hospitalName);
+            System.out.println("Patient ID: " + patientID);
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Ailment: " + ailment);
+        }
+    }
+}
+
